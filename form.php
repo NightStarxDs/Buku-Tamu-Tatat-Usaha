349fed707c561b87e0c02d3d54296750320921e6
Samuelsimorangkir-69 <arkanarivin@gmail.com>
2025-10-13 08:51:59 +0700

    Create form.php

---
+++ form.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Login admin</title>
+    <link rel="stylesheet" href="style1.css">
+</head>
+<body>
+    <div class="form-container">
+           <h2>Login Admin</h2>
+    <form id="registrationForm">
+        <div class="form-group">
+            <label for="name">id:</label>
+            <input type="text" id="name" name="name" placeholder="Masukkan nama">
+            <div class="error-massage" id="nameError"> Nama tidak boleh kosong</div>
+        </div>
+        <div class="form-group">
+            <label for="email">Email:</label>
+            <input type="email" id="email" name="email" placeholder="Masukkan alamat email yang valid">
+            <div class="error-massage" id="emailError">Nama tidak boleh kosong</div>
+        </div>
+        <div class="form-group">
+                <label for="password">Kata Sandi:</label>
+                <input type="password" id="password" name="password" placeholder="Masukkan kata sandi">
+                <div class="error-massage" id="passwordError">Kata sandi harus minimal 6 karakter</div>
+            </label>
+        </div>
+        <button type="submit" class="submit-btn">Daftar</button>
+     </form>
+    </div>
+    <script src="script1.js"></script>
+</body>
+</html>
