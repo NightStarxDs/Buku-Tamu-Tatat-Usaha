39d152b37fac8e029e731e3721bd5558f25e5d6c
Dwi Agung Willy Anto <Dwiagung0515@gmail.com>
2025-10-19 12:55:00 +0700

    Templating
    Sorting and Rephrase the code to make it easier

---
+++ data_kunjungan.php
@@ -0,0 +1,59 @@
+<?php include 'admin_template/header.php'; ?>
+
+<body class="page-top">
+
+    <!-- Page Wrapper -->
+    <div id="wrapper">
+        <?php include 'admin_template/sidebar.php'; ?>
+
+        <!-- Content Wrapper -->
+        <div id="content-wrapper" class="d-flex flex-column">
+
+            <!-- Main Content -->
+            <div id="content">
+                <?php include 'admin_template/topbar.php'; ?>
+
+                <!-- Content -->
+                <div class="container-fluid">
+
+                    <!-- Page Heading -->
+                    <div class="d-sm-flex align-items-center justify-content-between mb-4">
+                        <h1 class="h3 mb-0 text-gray-800">Data Kunjungan</h1>
+                        <a href="#" class="d-none d-sm-inline-block btn btn-sm btn-primary shadow-sm">
+                            <i class="fas fa-download fa-sm text-white-50"></i> Generate Report</a>
+                    </div>
+
+                    <div class="table">
+
+                        <table class="table table-bordered table-striped table-hover table-condensed">
+                            <thead class="thead thead-dark">
+                                <tr>
+                                    <th>Hello</th>
+                                    <th>A</th>
+                                    <th>A</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                <tr>
+                                    <td scope="row">A</td>
+                                    <td>A</td>
+                                    <td>A</td>
+                                </tr>
+                                <tr>
+                                    <td scope="row">A</td>
+                                    <td>A</td>
+                                    <td>A</td>
+                                </tr>
+                            </tbody>
+                        </table>
+                    </div>
+                </div>
+                <!-- /.container-fluid -->
+            </div>
+            <!-- End of Main Content -->
+
+            <!-- Footer -->
+            <?php include 'admin_template/footer.php'; ?>
+
+
+</body>
