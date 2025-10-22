2380f98431795e98036a7c34f7dcd60611de4043
Dwi Agung Willy Anto <Dwiagung0515@gmail.com>
2025-10-22 09:06:19 +0700

    Changes
    a

--- data_kunjungan.php
+++ data_kunjungan.php
@@ -19,132 +19,68 @@
                     <!-- Page Heading -->
                     <div class="d-sm-flex align-items-center justify-content-between mb-4">
                         <h1 class="h3 mb-0 text-gray-800">Data Kunjungan</h1>
-                        <a href="#" class="d-none d-sm-inline-block btn btn-sm btn-primary shadow-sm">
-                            <i class="fas fa-download fa-sm text-white-50"></i> Generate Report</a>
                     </div>
 
-                    <div>
+                    <!-- DataTales Example -->
+                    <div class="card shadow mb-4">
+                        <div class="card-header py-3">
+                            <h6 class="m-0 font-weight-bold text-primary">DataTables Example</h6>
+                        </div>
+                        <div class="card-body">
+                            <div class="table-responsive">
+                                <table class="table table-bordered" id="dataTable" width="100%" cellspacing="0">
+                                    <thead>
+                                        <tr>
+                                            <th>Nama</th>
+                                            <th>Nama Instansi</th>
+                                            <th>Perihal Kunjungan</th>
+                                            <th>Tanggal Kunjungan</th>
+                                            <th>Status</th>
+                                            <th>Aksi</th>
+                                        </tr>
+                                    </thead>
+                                    <tfoot>
+                                        <tr>
+                                            <th>Nama</th>
+                                            <th>Nama Instansi</th>
+                                            <th>Perihal Kunjungan</th>
+                                            <th>Tanggal Kunjungan</th>
+                                            <th>Status</th>
+                                            <th>Aksi</th>
+                                        </tr>
+                                    </tfoot>
 
-                        <table class="table table-bordered table-hover table-condensed">
-                            <thead class="thead bg-primary text-white">
-                                <tr>
-                                    <th class="text-center">No</th>
-                                    <th class="text-center">Nama</th>
-                                    <th class="text-center">Nama Instansi</th>
-                                    <th class="text-center">Perihal Kunjungan</th>
-                                    <th class="text-center">Tanggal Kunjungan</th>
-                                    <th colspan="3" class="text-center">Aksi</th>
-                                </tr>
-                            </thead>
-                            <tbody>
-                                <tr>
-                                    <td>1</td>
-                                    <td>Dwi Agung Willy Anto</td>
-                                    <td>SMKN 4 BATAM</td>
-                                    <td>Sosialisasi Lingkungan Kampus</td>
-                                    <td>25-10-2025</td>
-                                    <td><a href="" class="btn btn-primary" title="Info">
-                                            <i class=" fas fa-info px-1"></i>
-                                        </a>
-                                    </td>
-                                    <td><a href="" class="btn btn-warning" title="Edit">
-                                            <i class="fas fa-edit"></i>
-                                        </a>
-                                    </td>
-                                    <td><a href="" class="btn btn-danger" title="Hapus">
-                                            <i class="fas fa-trash"></i>
-                                        </a>
-                                    </td>
-                                </tr>
-                                <tr>
-                                    <td>2</td>
-                                    <td>Virgiawan Aziz Listianto</td>
-                                    <td>SMKN 4 BATAM</td>
-                                    <td>Sosialisasi Lingkungan Kampus</td>
-                                    <td>25-10-2025</td>
-                                    <td><a href="" class="btn btn-primary" title="Info">
-                                            <i class=" fas fa-info px-1"></i>
-                                        </a>
-                                    </td>
-                                    <td><a href="" class="btn btn-warning" title="Edit">
-                                            <i class="fas fa-edit"></i>
-                                        </a>
-                                    </td>
-                                    <td><a href="" class="btn btn-danger" title="Hapus">
-                                            <i class="fas fa-trash"></i>
-                                        </a>
-                                    </td>
-                                </tr>
+                                    <tbody>
+                                        <tr>
+                                            <td>Tiger Nixon</td>
+                                            <td>System Architect</td>
+                                            <td>Edinburgh</td>
+                                            <td>61</td>
+                                            <td>2011/04/25</td>
+                                            <td class="d-flex justify-content-between">
+                                                <a href="" class="btn btn-primary" title="Info">
+                                                    <i class=" fas fa-info px-1"></i>
+                                                </a>
+                                                <a href="" class="btn btn-warning" title="Edit">
+                                                    <i class="fas fa-edit"></i>
+                                                </a>
 
-                                <tr>
-                                    <td>3</td>
-                                    <td>Zaid Hasbiya Abrar</td>
-                                    <td>SMKN 4 BATAM</td>
-                                    <td>Sosialisasi Lingkungan Kampus</td>
-                                    <td>25-10-2025</td>
-                                    <td><a href="" class="btn btn-primary" title="Info">
-                                            <i class=" fas fa-info px-1"></i>
-                                        </a>
-                                    </td>
-                                    <td><a href="" class="btn btn-warning" title="Edit">
-                                            <i class="fas fa-edit"></i>
-                                        </a>
-                                    </td>
-                                    <td><a href="" class="btn btn-danger" title="Hapus">
-                                            <i class="fas fa-trash"></i>
-                                        </a>
-                                    </td>
-                                </tr>
-
-                                <tr>
-                                    <td>4</td>
-                                    <td>Bapak Budi Santoso</td>
-                                    <td>Pemerintah Kota Sejahtera</td>
-                                    <td>Studi Banding Tata Kelola Pelayanan Publik Berbasis Digital</td>
-                                    <td>27-10-2025</td>
-                                    <td><a href="" class="btn btn-primary" title="Info">
-                                            <i class=" fas fa-info px-1"></i>
-                                        </a>
-                                    </td>
-                                    <td><a href="" class="btn btn-warning" title="Edit">
-                                            <i class="fas fa-edit"></i>
-                                        </a>
-                                    </td>
-                                    <td><a href="" class="btn btn-danger" title="Hapus">
-                                            <i class="fas fa-trash"></i>
-                                        </a>
-                                    </td>
-                                </tr>
-
-                                <tr>
-                                    <td>5</td>
-                                    <td>Ibu Rina Kusuma</td>
-                                    <td>Universitas Maju Bangsa</td>
-                                    <td>Kunjungan Industri dan Diskusi Mekanisme Pengawasan Keuangan</td>
-                                    <td>28-10-2025</td>
-                                    <td><a href="" class="btn btn-primary" title="Info">
-                                            <i class=" fas fa-info px-1"></i>
-                                        </a>
-                                    </td>
-                                    <td><a href="" class="btn btn-warning" title="Edit">
-                                            <i class="fas fa-edit"></i>
-                                        </a>
-                                    </td>
-                                    <td><a href="" class="btn btn-danger" title="Hapus">
-                                            <i class="fas fa-trash"></i>
-                                        </a>
-                                    </td>
-                                </tr>
-                            </tbody>
-                        </table>
+                                                <a href="" class="btn btn-danger" title="Hapus">
+                                                    <i class="fas fa-trash"></i>
+                                                </a>
+                                            </td>
+                                        </tr>
+                                    </tbody>
+                                </table>
+                            </div>
+                        </div>
                     </div>
+                    <!-- /.container-fluid -->
                 </div>
-                <!-- /.container-fluid -->
-            </div>
-            <!-- End of Main Content -->
+                <!-- End of Main Content -->
 
-            <!-- Footer -->
-            <?php include 'dashboard_template/footer.php'; ?>
+                <!-- Footer -->
+                <?php include 'dashboard_template/footer.php'; ?>
 
 
 </body>
--- pending.php
+++ pending.php
@@ -18,83 +18,58 @@
 
                     <!-- Page Heading -->
                     <div class="d-sm-flex align-items-center justify-content-between mb-4">
-                        <h1 class="h3 mb-0 text-gray-800">Pendings</h1>
-                        <a href="#" class="d-none d-sm-inline-block btn btn-sm btn-primary shadow-sm">
-                            <i class="fas fa-download fa-sm text-white-50"></i> Generate Report</a>
+                        <h1 class="h3 mb-0 text-gray-800">Data Kunjungan</h1>
                     </div>
 
-                    <div>
+                    <!-- DataTales Example -->
+                    <div class="card shadow mb-4">
+                        <div class="card-header py-3">
+                            <h6 class="m-0 font-weight-bold text-primary">DataTables Example</h6>
+                        </div>
+                        <div class="card-body">
+                            <div class="table-responsive">
+                                <table class="table table-bordered" id="dataTable" width="100%" cellspacing="0">
+                                    <thead>
+                                        <tr>
+                                            <th>Nama</th>
+                                            <th>Nama Instansi</th>
+                                            <th>Office</th>
+                                            <th>Age</th>
+                                            <th>Start date</th>
+                                            <th>Salary</th>
+                                        </tr>
+                                    </thead>
+                                    <tfoot>
+                                        <tr>
+                                            <th>Name</th>
+                                            <th>Position</th>
+                                            <th>Office</th>
+                                            <th>Age</th>
+                                            <th>Start date</th>
+                                            <th>Salary</th>
+                                        </tr>
+                                    </tfoot>
 
-                        <table class="table table-bordered table-hover table-condensed">
-                            <thead class="thead bg-warning text-white">
-                                <tr>
-                                    <th class="text-center">No</th>
-                                    <th class="text-center">Nama</th>
-                                    <th class="text-center">Nama Instansi</th>
-                                    <th class="text-center">Perihal Kunjungan</th>
-                                    <th class="text-center">Tanggal Kunjungan</th>
-                                    <th colspan="2" class="text-center">Aksi</th>
-                                </tr>
-                            </thead>
-                            <tbody>
-                                <tr>
-                                    <td>1</td>
-                                    <td>Dwi Agung Willy Anto</td>
-                                    <td>SMKN 4 BATAM</td>
-                                    <td>Sosialisasi Lingkungan Kampus</td>
-                                    <td>25-10-2025</td>
-                                    <td class="text-center"><a href="#" class="btn btn-success btn-sm" title="Terima"><i class="fas fa-check"></i></a></td>
-                                    <td class="text-center"><a href="#" class="btn btn-danger btn-sm px-2" title="Tolak"><i class="fas fa-times"></i><a></td>
-                                </tr>
-                                <tr>
-                                    <td>2</td>
-                                    <td>Virgiawan Aziz Listianto</td>
-                                    <td>SMKN 4 BATAM</td>
-                                    <td>Sosialisasi Lingkungan Kampus</td>
-                                    <td>25-10-2025</td>
-                                    <td class="text-center"><a href="#" class="btn btn-success btn-sm" title="Terima"><i class="fas fa-check"></i></a></td>
-                                    <td class="text-center"><a href="#" class="btn btn-danger btn-sm px-2" title="Tolak"><i class=" fas fa-times"></i><a></td>
-                                </tr>
-
-                                <tr>
-                                    <td>3</td>
-                                    <td>Zaid Hasbiya Abrar</td>
-                                    <td>SMKN 4 BATAM</td>
-                                    <td>Sosialisasi Lingkungan Kampus</td>
-                                    <td>25-10-2025</td>
-                                    <td class="text-center"><a href="#" class="btn btn-success btn-sm" title="Terima"><i class="fas fa-check"></i></a></td>
-                                    <td class="text-center"><a href="#" class="btn btn-danger btn-sm px-2" title="Tolak"><i class="fas fa-times"></i><a></td>
-                                </tr>
-
-                                <tr>
-                                    <td>4</td>
-                                    <td>Bapak Budi Santoso</td>
-                                    <td>Pemerintah Kota Sejahtera</td>
-                                    <td>Studi Banding Tata Kelola Pelayanan Publik Berbasis Digital</td>
-                                    <td>27-10-2025</td>
-                                    <td class="text-center"><a href="#" class="btn btn-success btn-sm" title="Terima"><i class="fas fa-check"></i></a></td>
-                                    <td class="text-center"><a href="#" class="btn btn-danger btn-sm px-2" title="Tolak"><i class="fas fa-times"></i><a></td>
-                                </tr>
-
-                                <tr>
-                                    <td>5</td>
-                                    <td>Ibu Rina Kusuma</td>
-                                    <td>Universitas Maju Bangsa</td>
-                                    <td>Kunjungan Industri dan Diskusi Mekanisme Pengawasan Keuangan</td>
-                                    <td>28-10-2025</td>
-                                    <td class="text-center"><a href="#" class="btn btn-success btn-sm" title="Terima"><i class="fas fa-check"></i></a></td>
-                                    <td class="text-center"><a href="#" class="btn btn-danger btn-sm px-2" title="Tolak"><i class="fas fa-times"></i><a></td>
-                                </tr>
-                            </tbody>
-                        </table>
+                                    <tbody>
+                                        <tr>
+                                            <td>Tiger Nixon</td>
+                                            <td>System Architect</td>
+                                            <td>Edinburgh</td>
+                                            <td>61</td>
+                                            <td>2011/04/25</td>
+                                            <td>$320,800</td>
+                                        </tr>
+                                    </tbody>
+                                </table>
+                            </div>
+                        </div>
                     </div>
+                    <!-- /.container-fluid -->
                 </div>
-                <!-- /.container-fluid -->
-            </div>
-            <!-- End of Main Content -->
+                <!-- End of Main Content -->
 
-            <!-- Footer -->
-            <?php include 'dashboard_template/footer.php'; ?>
+                <!-- Footer -->
+                <?php include 'dashboard_template/footer.php'; ?>
 
 
 </body>
