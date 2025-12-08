91fd0a69b7c74bf787b8acb6bfdb345d82c04035
Samuelsimorangkir-69 <jujuna258099@gmail.com>
2025-12-08 10:28:36 +0700

    Update proses_login.php

--- proses_login.php
+++ proses_login.php
@@ -2,15 +2,12 @@
 session_start();
 include "koneksi.php";
 
-// ambil data dari form
-$username = $_POST['username'];
-$password = $_POST['password'];
+$username = md5($_POST['username']);
+$password = md5($_POST['password']);
 
-// query cek user
 $query = "SELECT * FROM users WHERE username='$username' AND password='$password'";
 $result = mysqli_query($koneksi, $query);
 
-// cek hasil
 if (mysqli_num_rows($result) > 0) {
     $data = mysqli_fetch_assoc($result);
 
