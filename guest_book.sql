136a6cae4715afa85e8aee4d100c7cbc94aceb33
NightStarxDs <Dwiagung0515@gmail.com>
2025-12-29 13:21:07 +0700

    database update

--- guest_book.sql
+++ guest_book.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Dec 22, 2025 at 04:36 AM
+-- Generation Time: Dec 29, 2025 at 05:52 AM
 -- Server version: 10.4.32-MariaDB
 -- PHP Version: 8.2.12
 
@@ -31,7 +31,7 @@ CREATE TABLE `staf` (
   `id_staf` int(11) NOT NULL,
   `staf_name` varchar(255) NOT NULL,
   `staf_email` varchar(255) NOT NULL,
-  `staf_number` int(11) NOT NULL,
+  `staf_number` varchar(255) NOT NULL,
   `staf_unit` enum('dosen_informatika','dosen_manajemen','dosen_elektro','dosen_mesin') NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
@@ -40,26 +40,26 @@ CREATE TABLE `staf` (
 --
 
 INSERT INTO `staf` (`id_staf`, `staf_name`, `staf_email`, `staf_number`, `staf_unit`) VALUES
-(1, 'Adhitomo Wirawan, S.ST., M.BA', '', 0, 'dosen_manajemen'),
-(2, 'Aditya Wirangga Pratama, M.AB', '', 0, 'dosen_manajemen'),
-(3, 'Afriyanti Hasanah, S.S.T., M.Sc', '', 0, 'dosen_manajemen'),
-(4, 'Alfonsa Dian Sumarna, S.E., M.Si', '', 0, 'dosen_manajemen'),
-(5, 'Andi Erna Mulyana, S.T., M.Sc', '', 0, 'dosen_manajemen'),
-(6, 'Dwi Ely Kurniawan, S.Pd., M.Kom', '', 0, 'dosen_informatika'),
-(7, 'Evaliata Br. Sembiring, S.Kom., M.Cs', '', 0, 'dosen_informatika'),
-(8, 'Nur Zahrati Janah, S.Kom, M.Sc', '', 0, 'dosen_informatika'),
-(9, 'Swono Sibagariang, S.Kom., M.Kom', '', 0, 'dosen_informatika'),
-(10, 'Yeni Rokhayati, S.Si., M.Sc', '', 0, 'dosen_informatika'),
-(11, 'Abdullah Sani, S.ST, M.Sc', '', 0, 'dosen_mesin'),
-(12, 'Aditya Gautama Darmoyono, S.T., M.T.', '', 0, 'dosen_elektro'),
-(13, 'Adlian Jefiza, S.Pd., M.T.', '', 0, 'dosen_elektro'),
-(14, 'Ahmad Riyad Firdaus, S.Si., M.T., Ph.D', '', 0, 'dosen_elektro'),
-(15, 'Arif Febriansyah Juwito, S.T., M.Eng', '', 0, 'dosen_elektro'),
-(16, 'Adhe Aryswan, S.Pd., M.Si.', '', 0, 'dosen_mesin'),
-(17, 'Agustinus Herwien Gunawan', '', 0, 'dosen_mesin'),
-(18, 'Ananta Setiadi, S.T.', '', 0, 'dosen_mesin'),
-(19, 'Andrew W P Mantik, S.T', '', 0, 'dosen_mesin'),
-(20, 'Annisa Fyona, S.K.M., M.K.K.K', '', 0, 'dosen_mesin');
+(1, 'Adhitomo Wirawan, S.ST., M.BA', 'dwiagung0515@gmail.com', '+62895337342838', 'dosen_manajemen'),
+(2, 'Aditya Wirangga Pratama, M.AB', 'dwiagung0515@gmail.com', '+62895337342838', 'dosen_manajemen'),
+(3, 'Afriyanti Hasanah, S.S.T., M.Sc', 'dwiagung0515@gmail.com', '+62895337342838', 'dosen_manajemen'),
+(4, 'Alfonsa Dian Sumarna, S.E., M.Si', 'dwiagung0515@gmail.com', '+62895337342838', 'dosen_manajemen'),
+(5, 'Andi Erna Mulyana, S.T., M.Sc', 'dwiagung0515@gmail.com', '+62895337342838', 'dosen_manajemen'),
+(6, 'Dwi Ely Kurniawan, S.Pd., M.Kom', 'dwiagung0515@gmail.com', '+62895337342838', 'dosen_informatika'),
+(7, 'Evaliata Br. Sembiring, S.Kom., M.Cs', 'dwiagung0515@gmail.com', '+62895337342838', 'dosen_informatika'),
+(8, 'Nur Zahrati Janah, S.Kom, M.Sc', 'dwiagung0515@gmail.com', '+62895337342838', 'dosen_informatika'),
+(9, 'Swono Sibagariang, S.Kom., M.Kom', 'dwiagung0515@gmail.com', '+62895337342838', 'dosen_informatika'),
+(10, 'Yeni Rokhayati, S.Si., M.Sc', 'dwiagung0515@gmail.com', '+62895337342838', 'dosen_informatika'),
+(11, 'Abdullah Sani, S.ST, M.Sc', 'dwiagung0515@gmail.com', '+62895337342838', 'dosen_mesin'),
+(12, 'Aditya Gautama Darmoyono, S.T., M.T.', 'dwiagung0515@gmail.com', '+62895337342838', 'dosen_elektro'),
+(13, 'Adlian Jefiza, S.Pd., M.T.', 'dwiagung0515@gmail.com', '+62895337342838', 'dosen_elektro'),
+(14, 'Ahmad Riyad Firdaus, S.Si., M.T., Ph.D', 'dwiagung0515@gmail.com', '+62895337342838', 'dosen_elektro'),
+(15, 'Arif Febriansyah Juwito, S.T., M.Eng', 'dwiagung0515@gmail.com', '+62895337342838', 'dosen_elektro'),
+(16, 'Adhe Aryswan, S.Pd., M.Si.', 'dwiagung0515@gmail.com', '+62895337342838', 'dosen_mesin'),
+(17, 'Agustinus Herwien Gunawan', 'dwiagung0515@gmail.com', '+62895337342838', 'dosen_mesin'),
+(18, 'Ananta Setiadi, S.T.', 'dwiagung0515@gmail.com', '+62895337342838', 'dosen_mesin'),
+(19, 'Andrew W P Mantik, S.T', 'dwiagung0515@gmail.com', '+62895337342838', 'dosen_mesin'),
+(20, 'Annisa Fyona, S.K.M., M.K.K.K', 'dwiagung0515@gmail.com', '+62895337342838', 'dosen_mesin');
 
 -- --------------------------------------------------------
 
@@ -70,7 +70,7 @@ INSERT INTO `staf` (`id_staf`, `staf_name`, `staf_email`, `staf_number`, `staf_u
 CREATE TABLE `unit` (
   `id_unit` int(11) NOT NULL,
   `unit_name` varchar(255) NOT NULL,
-  `unit_number` int(11) NOT NULL,
+  `unit_number` varchar(255) NOT NULL,
   `unit_email` varchar(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
@@ -79,22 +79,22 @@ CREATE TABLE `unit` (
 --
 
 INSERT INTO `unit` (`id_unit`, `unit_name`, `unit_number`, `unit_email`) VALUES
-(1, 'Pusat Penjaminan Mutu (P4M)', 0, ''),
-(2, 'Penelitian & Pengabdian Masyarakat (P3M)', 0, ''),
-(3, 'Satuan Pengawas Internal (SPI)', 0, ''),
-(4, 'UPA Perpustakaan', 0, ''),
-(5, 'UPA Perbaikan & Perawatan', 0, ''),
-(6, 'UPA Teknologi Infomasi & Komunikasi', 0, ''),
-(7, 'Pengembangan Karir & Kewirausahaan', 0, ''),
-(8, 'Sub Bagian Akademik', 0, ''),
-(9, 'Kemahasiswaan', 0, ''),
-(10, 'Sub Bagian Umum', 0, ''),
-(11, 'Humas & Kerjasama', 0, ''),
-(12, 'BMN & Pengadaan', 0, ''),
-(13, 'Organisasi & SDM', 0, ''),
-(14, 'Keuangan', 0, ''),
-(15, 'Perencanaan', 0, ''),
-(16, 'Shilau', 0, '');
+(1, 'Pusat Penjaminan Mutu (P4M)', '+62895337342838', 'dwiagung0515@gmail.com'),
+(2, 'Penelitian & Pengabdian Masyarakat (P3M)', '+62895337342838', 'dwiagung0515@gmail.com'),
+(3, 'Satuan Pengawas Internal (SPI)', '+62895337342838', 'dwiagung0515@gmail.com'),
+(4, 'UPA Perpustakaan', '+62895337342838', 'dwiagung0515@gmail.com'),
+(5, 'UPA Perbaikan & Perawatan', '+62895337342838', 'dwiagung0515@gmail.com'),
+(6, 'UPA Teknologi Infomasi & Komunikasi', '+62895337342838', 'dwiagung0515@gmail.com'),
+(7, 'Pengembangan Karir & Kewirausahaan', '+62895337342838', 'dwiagung0515@gmail.com'),
+(8, 'Sub Bagian Akademik', '+62895337342838', 'dwiagung0515@gmail.com'),
+(9, 'Kemahasiswaan', '+62895337342838', 'dwiagung0515@gmail.com'),
+(10, 'Sub Bagian Umum', '+62895337342838', 'dwiagung0515@gmail.com'),
+(11, 'Humas & Kerjasama', '+62895337342838', 'dwiagung0515@gmail.com'),
+(12, 'BMN & Pengadaan', '+62895337342838', 'dwiagung0515@gmail.com'),
+(13, 'Organisasi & SDM', '+62895337342838', 'dwiagung0515@gmail.com'),
+(14, 'Keuangan', '+62895337342838', 'dwiagung0515@gmail.com'),
+(15, 'Perencanaan', '+62895337342838', 'dwiagung0515@gmail.com'),
+(16, 'Shilau', '+62895337342838', 'dwiagung0515@gmail.com');
 
 -- --------------------------------------------------------
 
@@ -171,7 +171,15 @@ INSERT INTO `visit_data` (`id`, `guest_name`, `email`, `phone_number`, `company_
 (40, 'Zaky', 'dwiagung0515@gmail.com', '+62895337342838', 'Visilog', 'Janji_temu_unit', 'Bertemu Unit Penjamin Mutu', 1, NULL, '2025-12-19', '10:36:08', '10:38:08', 'Done'),
 (41, 'Fauzan', 'dwiagung0515@gmail.com', '+62895337342838', 'Visilog', 'Janji_temu_unit', 'Bertemu Unit Penjamin Mutu', 1, NULL, '2025-12-20', '10:36:08', '10:38:08', 'Done'),
 (42, 'Agung', 'dwiagung0515@gmail.com', '+62895337342838', 'Visilog', 'Janji_temu_unit', 'Bertemu Unit Penjamin Mutu', 1, NULL, '2025-12-21', '10:36:08', '10:38:08', 'Done'),
-(43, 'Dwi Agung', 'dwiagung0515@gmail.com', '62895337342838', 'Visilog', 'Urusan_umum', 'Mengurus Data Diri', NULL, NULL, '2026-12-30', '17:00:00', '18:00:00', 'Upcoming');
+(65, 'Dwi Agung Willy Anto', 'dwiagung0515@gmail.com', '+62895337342838', 'Visilog', 'Urusan_umum', 'Mengurus Hal Umum', NULL, NULL, '2026-12-24', '19:59:00', '21:59:00', 'Upcoming'),
+(66, 'Virgiawan Aziz Listianto', 'dwiagung0515@gmail.com', '+62895337342838', 'Visilog', 'Urusan_umum', 'Mengurus Hal Umum', NULL, NULL, '2026-12-24', '19:59:00', '21:59:00', 'Upcoming'),
+(67, 'Arys Apriatna Ananda', 'dwiagung0515@gmail.com', '+62895337342838', 'Visilog', 'Urusan_umum', 'Mengurus Hal Umum', NULL, NULL, '2026-12-24', '19:59:00', '21:59:00', 'Upcoming'),
+(68, 'Kevin Febriano', 'dwiagung0515@gmail.com', '+62895337342838', 'Visilog', 'Janji_temu_staf', 'Mengurus Hal Umum', NULL, 8, '2026-12-24', '19:59:00', '21:59:00', 'Pending'),
+(69, 'Dimas Pratama Putra', 'dwiagung0515@gmail.com', '+62895337342838', 'Visilog', 'Janji_temu_staf', 'Mengurus Hal Umum', NULL, 8, '2026-12-24', '19:59:00', '21:59:00', 'Pending'),
+(70, 'M Davawin Cahyono', 'dwiagung0515@gmail.com', '+62895337342838', 'Visilog', 'Janji_temu_staf', 'Mengurus Hal Umum', NULL, 8, '2026-12-24', '19:59:00', '21:59:00', 'Pending'),
+(71, 'M Safamal Jovanda', 'dwiagung0515@gmail.com', '+62895337342838', 'Visilog', 'Janji_temu_staf', 'Mengurus Hal Umum', NULL, 8, '2026-12-24', '19:59:00', '00:00:00', 'Rejected'),
+(72, 'Rizky Ramadhani', 'dwiagung0515@gmail.com', '+62895337342838', 'Visilog', 'Janji_temu_staf', 'Mengurus Hal Umum', NULL, 8, '2026-12-24', '19:59:00', '00:00:00', 'Rejected'),
+(73, 'Zaid Hasbiya Abrar', 'dwiagung0515@gmail.com', '+62895337342838', 'Visilog', 'Janji_temu_staf', 'Mengurus Hal Umum', NULL, 8, '2026-12-24', '19:59:00', '00:00:00', 'Rejected');
 
 --
 -- Indexes for dumped tables
@@ -229,7 +237,7 @@ ALTER TABLE `users`
 -- AUTO_INCREMENT for table `visit_data`
 --
 ALTER TABLE `visit_data`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=44;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=74;
 
 --
 -- Constraints for dumped tables
